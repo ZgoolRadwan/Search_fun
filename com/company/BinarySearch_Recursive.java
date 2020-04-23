b6b21a39b19d68b9c808ff6ebd87e942ecc7e69d
Radwan <ammanjordan1997@gmail.com>
2020-04-23 11:02:33 +0300

    binary search using loop

--- com/company/BinarySearch_Recursive.java
+++ com/company/BinarySearch_Recursive.java
@@ -4,18 +4,19 @@ import java.util.Arrays;
 
 public class BinarySearch_Recursive {
 
-    int binarySearch(int arr[], int l, int r, int x)
+    int binarySearch(int arr[], int x)
     {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] == x)
+                return m;
 
-        if (r >= l) {
-            int mid = l + (r - l) / 2;
-            if (arr[mid] == x)
-                return mid;
-            if (arr[mid] > x)
-                return binarySearch(arr, l, mid - 1, x);
-            return binarySearch(arr, mid + 1, r, x);
+            if (arr[m] < x)
+                l = m + 1;
+            else
+                r = m - 1;
         }
         return -1;
-
     }
 }
