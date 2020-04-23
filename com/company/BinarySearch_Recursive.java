38ff7a005e8bb50d8cd1104956123bac88cb59da
Radwan <ammanjordan1997@gmail.com>
2020-04-23 11:00:44 +0300

    Binary search

---
+++ com/company/BinarySearch_Recursive.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class BinarySearch_Recursive {
+
+    int binarySearch(int arr[], int l, int r, int x)
+    {
+
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x)
+                return mid;
+            if (arr[mid] > x)
+                return binarySearch(arr, l, mid - 1, x);
+            return binarySearch(arr, mid + 1, r, x);
+        }
+        return -1;
+
+    }
+}
